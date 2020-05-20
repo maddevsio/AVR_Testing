c13e79dc6a7fe30b8b33c39bb331f5854557b814
Oxide <a.ghost.gamer@gmail.com>
2020-05-21 00:32:19 +0600

    Makefile fix, added board source, added firmware wrapper

---
+++ src/tests/sim/obj-x86_64-linux-gnu/board_adcToTemp.d
@@ -0,0 +1,15 @@
+obj-x86_64-linux-gnu/board_adcToTemp.o: board_adcToTemp.c \
+ /home/oxide/simavr/simavr/sim/sim_avr.h \
+ /home/oxide/simavr/simavr/sim/sim_irq.h \
+ /home/oxide/simavr/simavr/sim/sim_interrupts.h \
+ /home/oxide/simavr/simavr/sim/sim_avr_types.h \
+ /home/oxide/simavr/simavr/sim/fifo_declare.h \
+ /home/oxide/simavr/simavr/sim/sim_cmds.h \
+ /home/oxide/simavr/simavr/sim/sim_cycle_timers.h \
+ /home/oxide/simavr/simavr/sim/sim_io.h \
+ /home/oxide/simavr/simavr/sim/sim_avr.h \
+ /home/oxide/simavr/simavr/sim/sim_regbit.h \
+ /home/oxide/simavr/simavr/sim/avr_ioport.h \
+ /home/oxide/simavr/simavr/sim/sim_elf.h \
+ /home/oxide/simavr/simavr/sim/avr/avr_mcu_section.h parts/hd44780.h \
+ /home/oxide/simavr/simavr/sim/sim_irq.h
